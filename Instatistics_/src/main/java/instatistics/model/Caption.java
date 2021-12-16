a11f3f31b9f019d4f04152c4707906a5b489d367
unknown <s1093619@studenti.univpm.it>
2021-12-16 09:56:25 +0100

    Aggiornamento classe Caption

--- Instatistics_/src/main/java/instatistics/model/Caption.java
+++ Instatistics_/src/main/java/instatistics/model/Caption.java
@@ -5,36 +5,21 @@ import org.json.simple.JSONObject;
 
 public class Caption extends FieldSuggest{
 	private JSONObject json = new JSONObject();
-	ArrayList <String> array = new ArrayList <String>();
-	ArrayList<String> hastag=new ArrayList<String>();
-	public Caption (ArrayList<String> array) {
+	ArrayList <Post> array = new ArrayList <Post>();
+	public Caption (ArrayList<Post> array) {
 		this.array=array;
 	}
-	public ArrayList<String> getHashtag(){
-		for(int i=0;i<array.size();i++) {
-			for(int j=0;j<array.get(i).length();j++) {
-				if(array.get(j).charAt(i) =='#') {hastag.get(j).substring(array.get(j).lastIndexOf('#')+1);
-			}
-		}
 	
-		
-		}
-		return hastag;
-	}
-/*	public String NumberOfRipetitionHas() {
+	public String NumberOfRepetition(String ObjectOfInterest) {
 		int cont=0;
 		for(int	i=0;i<array.size();i++) {
-			if(array.get(i).getCaption().toChar('#')) {
+			String s = array.get(i).getCaption(); //captin del post numero i
+			if(s.contains(ObjectOfInterest)) { //se la stringa s contiene l'hastag aumento il contatore
 			cont++;
 			}
 		}
 		return Integer.toString(cont); 
-	}
-	*/
-	@Override
-	public String NumberOfRepetition(String ObjectOfInterest) {
-		// TODO Auto-generated method stub
-		return null;
+	
 	}
 	@Override
 	public String Ranking() {
