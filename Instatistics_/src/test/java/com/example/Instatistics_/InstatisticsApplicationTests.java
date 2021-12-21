73b24d141ec3eaa358e3176fe1b6c6ce703c0e4b
unknown <s1093619@studenti.univpm.it>
2021-12-21 11:46:49 +0100

    Primo tentativo Junit

--- Instatistics_/src/test/java/com/example/Instatistics_/InstatisticsApplicationTests.java
+++ Instatistics_/src/test/java/com/example/Instatistics_/InstatisticsApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.Instatistics_;
 
-import org.junit.jupiter.api.Test;
+import instatistics.model.*;
+import junit.framework.*;
+
+import java.util.ArrayList;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InstatisticsApplicationTests {
-
-	@Test
-	void contextLoads() {
+class InstatisticsApplicationTests extends TestCase {
+	private Caption i1;
+	public InstatisticsApplicationTests(String name){
+		super(name);
+		}
+	
+	public void setUp(){
+		ArrayList<Post> pp = new ArrayList<Post>();
+		Caption i1 = new Caption(pp);
+		}
+	public void Suggestion(){
+		assertEquals(i1.Suggestion("sport"), "#nopainnogain");
+		assertEquals(i1.Suggestion("sport"), "#giornatainmovimento");
+		assertEquals(i1.Suggestion("sport"), "#sport");
+		}
+	public void tearDown(){};
+	public static TestSuite suite(){
+	TestSuite suite = new TestSuite();
+	suite.addTest(new InstatisticsApplicationTests("Suggestion"));
+	return suite;
 	}
-
 }
